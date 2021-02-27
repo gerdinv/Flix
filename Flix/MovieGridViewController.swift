8fb76fdb36b22bc3defcb144de55ffa6748b65bf
Gerdin <gerdinventura2@gmail.com>
2021-02-27 02:46:25 -0500

    Added the bonus features

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -65,14 +65,22 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        //Find the selected movie
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass the selected movie to the details View Controller
+        let detailsViewController = segue.destination as! SuperheroDetailsViewController
+        detailsViewController.movie = movie
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
-    */
-
 }
